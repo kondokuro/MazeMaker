89adfa569fb117bf715de156dd1c6671445bbd88
Patricia Urdaneta <dario.soto@gmail.com>
2021-03-16 07:02:23 -0400

    factory tests
    added a new factory test for the protected functions, as I go and think about the use cases

--- factory.py
+++ factory.py
@@ -8,17 +8,13 @@ def create_maze(paths: int, branches: int, branching_range: list, branch_length_
     - branching_range: min-max number of branches that can spread from a branch
     - branch_length_range: min-max number of areas a branch can have
     """
-    generated = Maze()
-    generated.add(_make_path(1))
+    generated = Maze([_make_path(1)])
     return generated
 
 
-def _make_path(length, connection: Area = None):
-    """Provides a branch of defined length areas with an entrance or exit.
-    If provided with a connection area, the path can have:
-    - a predefined entrance
-    - a predefined exit
-    - be connected to another branch of the maze
+def _make_path(length, connection: Area = None, one_way: bool = False):
+    """Provides a branch of defined length areas with portals, limited only by its length.
+    Provide a connection area to link branches
     """
     generated = Branch([Area(True)])
     return generated
@@ -26,4 +22,4 @@ def _make_path(length, connection: Area = None):
 
 def _make_branch(length, starting_area: Area):
     """Provides a branch of defined length from a starting area."""
-    return Branch()
+    return Branch([])
--- test_factory.py
+++ test_factory.py
@@ -1,8 +1,17 @@
-from factory import create_maze
-import pytest
+from factory import create_maze, _make_path
+from pytest import mark
 
 
-@pytest.mark.parametrize(
+@mark.parametrize("length", [1, 3, 5, 8, 13])
+def test_make_path_one_way_returns_one_portal_path_of_set_size(length):
+    path = _make_path(length, one_way=True)
+    portals = [area for area in path.areas if area.is_portal]
+    assert portals
+    assert len(portals) == 1
+    assert len(path.areas) == length
+
+
+@mark.parametrize(
     "entrances, exists, branches, min_length, max_length",
     [
         (1, 1, 1, 1, 1),
@@ -17,11 +26,6 @@ def test_create_maze_multiple_branches_returns_correct_maze(
     assert len(labyrinth.paths()) == entrances
     for branch in labyrinth.branches:
         assert False if (len(branch.areas) < min_length or len(branch.areas) > max_length) else True
-
-
-def test_create_maze_insufficient_branches_for_paths_returns_error_string():
-    labyrinth = create_maze(3, 3, 2, 3, 3)
-    assert labyrinth == "Insufficient branches for the requested entrances or exits"
 
 
 def test_create_maze_insufficient_branches_for_paths_returns_error_string():
