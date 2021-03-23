513188e4236d40683fab566821902ce5ef5a1bad
Patricia Urdaneta <dario.soto@gmail.com>
2021-03-23 19:47:17 -0400

    parts test updated

--- test_parts.py
+++ test_parts.py
@@ -2,49 +2,24 @@ from parts import Maze, Hall, Area
 import pytest
 
 
-@pytest.mark.parametrize("is_portal", [True, False])
-def test_area_is_portal(is_portal):
-    assert Area(is_portal).is_portal == is_portal
+@pytest.mark.parametrize("has_portal", [True, False])
+def test_area_is_portal_returns_area_portal_state(has_portal):
+    assert Area(has_portal).is_portal == has_portal
 
 
-@pytest.mark.parametrize("is_portal", [True, False])
-def test_area_to_dict_creates_dictionary_representation(is_portal):
-    area = Area(is_portal)
-    area_dict = area.to_dict()
-    assert area_dict
-    assert area_dict.get("id") == str(area.id)
-    assert area_dict.get("is_portal") == is_portal
-    assert area_dict.get("links") == area.links
-    assert len(area_dict.keys()) == 3
+@pytest.mark.parametrize("has_portal", [True, False])
+def test_hall_is_path_returns_hall_path_state(has_portal):
+    assert Hall([Area(has_portal)]).is_path == has_portal
 
 
-@pytest.mark.parametrize("is_path", [True, False])
-def test_branch_is_path_with_portals_returns_true(is_path):
-    assert Hall([Area(is_path)]).is_path == is_path
-
-
-@pytest.mark.parametrize("connect_count", [1, 3, 7])
-def test_branch_connections_returns_external_areas(connect_count):
-    an_area = Area()
-    branch = Hall([Area() for i in range(connect_count)])
-    branch.areas[0].connect(an_area)
+def test_hall_joints_returns_external_areas():
+    joint_area = Area()
+    external = Area()
+    joint_area.links.append(external)
+    branch = Hall([joint_area, Area()])
     assert len(branch.joints) == 1
-    assert an_area in branch.joints
-
-
-def test_branch_not_connected_has_connections_returns_false():
-    branch = Hall([Area(), Area()])
-    assert branch.is_linked() is False
-
-
-@pytest.mark.parametrize("is_path", [True, False])
-def test_branch_to_dict_creates_dictionary_representation(is_path):
-    branch = Hall([Area(is_path)])
-    branch_dict = branch.to_dict()
-    assert branch_dict
-    assert branch_dict.get("id") == str(branch.id)
-    assert branch_dict.get("is_path") == branch.is_path
-    assert len(branch_dict.get("areas")) == len(branch.areas)
+    assert external in branch.joints
+    assert external not in branch.areas
 
 
 def test_maze_branches_returns_branch_list():
@@ -65,15 +40,5 @@ def test_maze_paths_without_paths_returns_emtpy_list():
     assert Maze([Hall([Area(False)])]).paths == []
 
 
-def test_maze_branches_without_paths_returns_emtpy_list():
+def test_maze_branches_without_branches_returns_emtpy_list():
     assert Maze([Hall([Area(True)])]).branches == []
-
-
-def test_maze_to_dict_maze_creates_dictionary_representation():
-    path = [Hall([Area(True)])]
-    maze = Maze(path)
-    maze_dict = maze.to_dict()
-    assert maze_dict
-    assert maze_dict.get("id") == str(maze.id)
-    assert len(maze_dict.get("halls")) == len(maze.branches)
-    assert len(maze_dict.get("paths")) == len(maze.paths)
